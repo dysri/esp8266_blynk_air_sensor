666bb78f83df999121792645334e7a7e36104cc2
dysri <dylansrij@gmail.com>
2018-02-26 18:44:41 -0500

    Added watchdog timer for device hangs

--- esp8266_blynk_temp_sensor.ino
+++ esp8266_blynk_temp_sensor.ino
@@ -1,46 +1,29 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
+/*
+  Blynk WiFi Air Temperature/Humidity Sensor
 
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
+  Created: 26 February 2018
+  Last edit: 26 February 2018
+  Author: Dylan Sri-Jayantha
 
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
+  This code was written for ATmega328 wired with ESP8266 and DHT22 modules. Transmits
+  temperature and humidity data over WiFi via
 
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-
-  This example shows how to use ESP8266 Shield (with AT commands)
-  to connect your project to Blynk.
-
-  WARNING!
-    It's very tricky to get it working. Please read this article:
-    http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
-//#define BLYNK_PRINT Serial
+*/
 
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <avr/wdt.h>
 
 #define aref_voltage 3.3
 
 int dht22Pin = 2;
 int tmp36Pin = 0;
+
+// Time between transmissions
+unsigned long idlePeriod = 300000;
+unsigned long prevMillis = 0;
 
 DHT dht22(dht22Pin, DHT22);
 
@@ -62,7 +45,10 @@ BlynkTimer timer;
 ESP8266 wifi(&EspSerial);
 
 void timerEvent(){
-  
+  unsigned long currMillis = millis();
+  if ((unsigned long) (currMillis - prevMillis) < idlePeriod) wdt_reset();
+
+  else {
   // Take multiple TMP36 readings and average to smooth sensor signal
   int reading_total = 0;
   for (int i=0; i <= 9; i++) {
@@ -85,28 +71,18 @@ void timerEvent(){
   // Take reading on DHT22
   float hum = dht22.readHumidity();
   float tempf_22 = dht22.readTemperature(true);
-
-  // Print TMP36 values to Serial
-//  Serial.print("TMP36:\tVoltage: ");
-//  Serial.print(voltage); Serial.print(" V\tTemperature: ");
-//  Serial.print(temperatureF); Serial.println(" F");
-//  
-//  if (isnan(hum) || isnan(tempf_22)) {
-//    Serial.println("Failed to read from DHT sensor!");
-//    return;
-//  }
-//  Serial.print("DHT22:\tTemperature: "); Serial.print(tempf_22);
-//  Serial.print(" F\tHumidity: "); Serial.print(hum); Serial.println(" %");
   
   Blynk.virtualWrite(V0, temperatureF);
   Blynk.virtualWrite(V1, tempf_22);
   Blynk.virtualWrite(V2, hum);
+  prevMillis = currMillis;
+  wdt_reset();
+  }
 }
 
 void setup()
 {
-  // Debug console
-  //Serial.begin(9600);
+  wdt_enable(WDTO_8S);
   
   analogReference(EXTERNAL);
 
@@ -116,7 +92,8 @@ void setup()
   dht22.begin();
   
   Blynk.begin(auth, wifi, ssid, pass);
-  timer.setInterval(300000L, timerEvent);
+  timer.setInterval(5000L, timerEvent);
+  wdt_reset();
 }
 
 void loop()
